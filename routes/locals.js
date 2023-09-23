a61c087703e91742d9dd680f16ee3bfaacfff814
soeunjjang <hisoeun1123@naver.com>
2023-09-23 18:40:54 +0900

    ver04

--- routes/locals.js
+++ routes/locals.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* 지역검색 */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '홈페이지', pageName:'home.ejs' });
+  res.render('index', { title: '지역검색', pageName:'locals/search.ejs' });
 });
 
 module.exports = router;
