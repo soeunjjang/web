01bc5b2dba0c4ff835acc43d9f583d7af4bd96f3
soeunjjang <hisoeun1123@naver.com>
2023-10-14 18:44:54 +0900

    week7

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET 도서검색 page. */
+/* page :: 도서검색 */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '도서검색', pageName: 'books/search.ejs' });
+  res.render('index', { title: '도서검색', pageName: 'books/search.ejs'});
+});
+
+//장바구니 페이지
+router.get('/cart', function(req, res){
+  res.render('index', {title:'장바구니', pageName: 'books/cart.ejs'});
 });
 
 module.exports = router;
