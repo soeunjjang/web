d92f1cff9ac2449f44936156c33309845d3948ef
soeunjjang <hisoeun1123@naver.com>
2023-11-18 18:30:05 +0900

    week12

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/',function(req, res){
-    res.render('index',{title:'게시판', pageName: 'posts/list.ejs'})
+/* 게시글 페이지 */
+router.get('/', function(req, res) {
+  res.render('index', { title: '게시판', pageName :'posts/list.ejs' });
 });
 
-router.get('/insert',function(req, res){
-    res.render('index',{title:'글쓰기', pageName: 'posts/insert.ejs'})
+router.get('/insert', function(req, res) {
+  res.render('index', { title: '글쓰기', pageName :'posts/insert.ejs' });
+});
+
+router.get('/read', function(req, res) {
+  const id = req.query.id;
+  res.render('index', { title: '게시글 정보', pageName :'posts/read.ejs', id});
 });
 
 module.exports = router;
