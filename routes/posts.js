a073764389bcb0f38a6a622ba21e797946bec372
soeunjjang <hisoeun1123@naver.com>
2023-11-25 18:10:06 +0900

    week13

--- routes/posts.js
+++ routes/posts.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* 게시글 페이지 */
-router.get('/', function(req, res) {
-  res.render('index', { title: '게시판', pageName :'posts/list.ejs' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '게시판', pageName:'posts/list.ejs' });
 });
 
-router.get('/insert', function(req, res) {
-  res.render('index', { title: '글쓰기', pageName :'posts/insert.ejs' });
-});
+router.get('/insert', function(req, res, next) {
+    res.render('index', { title: '글쓰기', pageName:'posts/insert.ejs' });
+  });
 
-router.get('/read', function(req, res) {
-  const id = req.query.id;
-  res.render('index', { title: '게시글 정보', pageName :'posts/read.ejs', id});
-});
+  router.get('/read', function(req, res, next) {
+    const id=req.query.id;
+    res.render('index', { title: '게시글정보', pageName:'posts/read.ejs',id });
+  });
+
+  router.get('/update', function(req, res) {
+    const id=req.query.id;
+    res.render('index', { title: '게시글수정', pageName:'posts/update.ejs',id:id });
+  });
 
 module.exports = router;
